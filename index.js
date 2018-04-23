54cdf1020a287823e3b11af076e7171ea5e47e9f
Robert Laws <roblaws@me.com>
2018-04-23 11:28:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+function showCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`;
+  document.getElementById("commits").innerHTML = commitsList;
+}
+
+function getCommits(el) {
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", showCommits);
+  req.open("GET", "https://api.github.com/repos/octocat/" + name + "/commits");
+  req.send();
+}
+
 function showRepositories(event, data) {
   // console.log(this.responseText);
   var repos = JSON.parse(this.responseText);
@@ -8,7 +22,7 @@ function showRepositories(event, data) {
   // }
   // repoList += "</ul>";
 
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`;
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`;
 
   document.getElementById("repositories").innerHTML = repoList;
 }
