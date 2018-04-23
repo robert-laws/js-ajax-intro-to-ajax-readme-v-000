f66e27a02ad460071379e50d93fe3eefdeb684d6
Robert Laws <roblaws@me.com>
2018-04-23 11:22:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ function showRepositories(event, data) {
   // console.log(this.responseText);
   var repos = JSON.parse(this.responseText);
   console.log(repos);
-  let repoList = "<ul>";
-  for(var i = 0; i < this.responseText.length; i++) {
-    repoList += "<li>" + this.responseText[i]["name"] + "</li>";
-  }
-  repoList += "</ul>";
+  // let repoList = "<ul>";
+  // for(var i = 0; i < this.responseText.length; i++) {
+  //   repoList += "<li>" + this.responseText[i]["name"] + "</li>";
+  // }
+  // repoList += "</ul>";
+
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`;
+  
   document.getElementById("repositories").innerHTML = repoList;
 }
 
